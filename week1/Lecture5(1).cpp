d59296419f4e5fb34650174ea97fc3561682d9fd
gaurav sahu <118994378+Gauravsahu20@users.noreply.github.com>
2024-07-21 21:04:21 +0530

    Update Lecture5(1).cpp

--- week1/Lecture5(1).cpp
+++ week1/Lecture5(1).cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a,b,c,d,e;
-    cin>>a>>b>>c>>d>>e;
-    cout<<"numbers in reverse"<<e<<d<<c<<b<<a;
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a>b && c>d);
     return 0;
 }
