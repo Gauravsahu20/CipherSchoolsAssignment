7e67b56c9a630ab38971ff292c17b7fbf9b6a92d
gaurav sahu <118994378+Gauravsahu20@users.noreply.github.com>
2024-07-22 08:17:57 +0530

    Create lecture11.cpp

---
+++ week1/lecture11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int i=100;
+for(i=1;i<101;i++)
+{
+cin>>input;
+if(input==65)
+{
+cout<<"congrats you have guessed correct ";
+break;
+}
+}
+}
