6096a3150a571098073d98b58bcfb4a5a73b00c1
gaurav sahu <118994378+Gauravsahu20@users.noreply.github.com>
2024-07-22 13:14:55 +0530

    Create lecture12.cpp

---
+++ week1/lecture12.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int password;
+    cout<<"entre the password"<<endl;
+    cin>>password;
+    while(password<999999){
+        cout<<"the password is not correct,plz change it"<<endl;
+        cin>>password;
+        
+    }
+    cout<<"the entres pass is correct";
+    return 0;
+}
+ 
+#include <iostream>
+using namespace std;
+int main() {
+    int password;
+    cout<<"entre passss"<<endl;
+    do{
+        cin>>password;
+    }
+    while(password>999999)
+}
