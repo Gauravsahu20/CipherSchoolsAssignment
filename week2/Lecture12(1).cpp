910b32b677669d7bb3530f55d93340e8e990229d
gaurav sahu <118994378+Gauravsahu20@users.noreply.github.com>
2024-07-23 14:41:20 +0530

    Delete week2 directory

--- week2/Lecture12(1).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a;
-    float b;
-    cout<<"the address of a is : "<<&a<<endl;
-    cout<<"the address of a is : "<<&b<<endl;
-    return 0;
-}
--- week2/lecture11(1).cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    long long int a;
-    cout<<"the size of int is :"<<sizeof(int)<<endl;
-    cout<<"the size of int is :"<<sizeof(a)<<endl;
-    return 0;
-}
